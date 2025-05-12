1518c8ec30d3cdef409332f6749da248c7163f04
darek161 <chrzanowski90@gmail.com>
2025-05-12 09:35:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,5 +29,6 @@ def search():
         for item in results
     ])
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
